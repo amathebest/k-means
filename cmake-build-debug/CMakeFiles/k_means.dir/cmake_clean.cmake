ea00c46920690efd196a4117ee337b4685f33318
amathebest <amatori.matt@gmail.com>
2019-12-02 15:30:19 +0100

    Initial commit of the first project of the Parallel Compiuting course

---
+++ cmake-build-debug/CMakeFiles/k_means.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/k_means.dir/main.cpp.obj"
+  "k_means.exe"
+  "k_means.exe.manifest"
+  "k_means.pdb"
+  "libk_means.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/k_means.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
